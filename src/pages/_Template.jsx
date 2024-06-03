102cba7bea1d6b48be3443b05e284c2b17c85930
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-06-03 21:36:34 +1000

    feat: added footer component

--- src/pages/_Template.jsx
+++ src/pages/_Template.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function Template() {
 
@@ -7,6 +8,7 @@ export default function Template() {
         <>
             <Header />
             <Outlet />
+            <Footer />
         </>
     )
 }
